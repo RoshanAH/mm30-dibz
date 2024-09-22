4ea0f147861b930fca8b14f981af2f2cb3a29ad2
Daniel Philipov <batkodanny@gmail.com>
2024-09-22 07:30:55 -0500

    pigion

--- strategy/skibidi.py
+++ strategy/skibidi.py
@@ -35,22 +35,28 @@ def steer_away_from_point(plane: Plane, point: Vector) -> float:
     # Find the angle difference
     delta_theta = angle_diff(angle_to_point, plane.angle)
     # Find the maximum angle you can turn
-    max_theta = plane.stats.turn_speed
     # Return the steer
-    return -copysign(1, delta_theta / max_theta)
+    return -copysign(1, delta_theta)
+
+def steer_causes_crash(steer: float, plane: Plane, turns_ahead: int = 1) -> bool:
+    if turns_ahead <= 1:
+        return steer_crashes_plane(steer, plane)
+    next_plane = plane
+    next_plane.angle += steer * next_plane.stats.turn_speed
+    next_plane.position += next_plane.stats.speed * Vector(cos(radians(next_plane.angle)), sin(radians(next_plane.angle)))
+    return steer_causes_crash(steer, next_plane, turns_ahead - 1)
 
 
 class Skibidi:
-    let_it_rip: dict[str, bool] = {}
-    drop_radii: dict[str, float] = {}
     previous_targets: dict[str, tuple[str, int]] = {}
-    stagger_turns = 11
+    stagger_turns = 0
     
     def select_planes(self) -> dict[PlaneType, int]:
         return {
-            # PlaneType.STANDARD: 2,
-            PlaneType.THUNDERBIRD: 5,
-            # PlaneType.SCRAPYARD_RESCUE: 2,
+            PlaneType.STANDARD: 1,
+            PlaneType.THUNDERBIRD: 1,
+            PlaneType.FLYING_FORTRESS: 1,
+            PlaneType.PIGEON: 30,
         }
 
     def steer_to_point(self, plane: Plane, target: Vector) -> float:
@@ -63,24 +69,22 @@ class Skibidi:
     def steer_input(self, planes: dict[str, Plane]) -> dict[str, float]:
         response = {}
         my_planes = {id: plane for id, plane in planes.items() if plane.team == "friend"}
-        dists_to_each_other = {id1: {id2: (plane1.position - plane2.position).norm() for id2, plane2 in my_planes.items()} for id1, plane1 in my_planes.items()}
-        if not self.let_it_rip:
-            for id, plane in my_planes.items():
-                self.let_it_rip[id] = False
-                self.drop_radii[id] = degree_to_radius(plane.stats.turn_speed, plane.stats.speed)
+        hunters = {id: plane for id, plane in my_planes.items() if plane.type in [PlaneType.STANDARD, PlaneType.THUNDERBIRD]}
+        pigeons = {id: plane for id, plane in my_planes.items() if plane.type == PlaneType.PIGEON}
 
         not_my_planes = {id: plane for id, plane in planes.items() if plane.team == "enemy"}
         if self.stagger_turns > 0:
             self.stagger_turns -= 1
 
-        for id, plane in my_planes.items():
+        hunted_targets = {}
+        for id, plane in hunters.items():
             if self.stagger_turns > 0:
-                # response[id] = random() * 2 - 1
                 response[id] = copysign(1, plane.position.x * plane.position.y) * min(1, abs(plane.position.x/5))
-                # response[id] = -1 + 0.5 * (int(id) % 4)
                 continue
+
             def distance_heuristic(id):
                 return (diff := not_my_planes[id].position - plane.position).norm() * (abs(angle_diff(angle(diff), plane.angle))**(1/3))
+
             closest_enemies = sorted(not_my_planes, key=distance_heuristic)
 
             if id not in self.previous_targets:
@@ -90,27 +94,43 @@ class Skibidi:
 
             target_idx = 0
 
-            if turns_followed > 20 and len(closest_enemies) > 1:
+            if turns_followed > 100 and len(closest_enemies) > 1:
                 target_idx = 1
                 turns_followed = 0
-                # self.turn_for[id] = (0, 10)
 
+            warned = False
             for id_ in closest_enemies[target_idx:]:
-                steer = self.steer_to_point(plane, not_my_planes[id_].position)
-                if steer_crashes_plane(steer, plane):
+                enemy = not_my_planes[id_]
+                # future_enemy_position = enemy.position + enemy.stats.speed * Vector(cos(radians(enemy.angle)), sin(radians(enemy.angle)))
+                # averaged_enemy_position = (enemy.position + future_enemy_position)
+                # averaged_enemy_position = Vector(averaged_enemy_position.x / 2, averaged_enemy_position.y / 2)
+                steer = self.steer_to_point(plane, enemy.position)
+                if steer_causes_crash(steer, plane, 1):
+                    if not warned:
+                        print("WARN!", end="")
+                        warned = True
+                    else:
+                        print("!", end="")
                     continue
                 response[id] = steer
+                hunted_targets[id] = id_
+                if warned:
+                    print()
                 if id_ != previous_target:
                     self.previous_targets[id] = (id_, 1)
                 else:
                     self.previous_targets[id] = (id_, turns_followed + 1)
                 break
             
-            if id in response and steer_crashes_plane(response[id], plane):
-                print("CRASH WARNING: current steer =", response[id])
-                response[id] = steer_away_from_wall(plane)
+            if id not in response:
+                print("REDIRECTING")
+                steer = steer_away_from_wall(plane)
+                # if steer_crashes_plane(steer, plane):
+                    # print("WARN?",)
+                    # response[id] = -steer
+                    # continue
+                response[id] = steer
                 continue
-
 
             # TODO: detect whether other planes will hit you
             my_pos_next_turn = plane.position + plane.stats.speed * Vector(cos(radians(plane.angle)), sin(radians(plane.angle)))
@@ -118,13 +138,17 @@ class Skibidi:
             for id_ in closest_enemies[:5]:
                 enemy = not_my_planes[id_]
                 enemy_pos_next_turn = enemy.position + enemy.stats.speed * Vector(cos(radians(enemy.angle)), sin(radians(enemy.angle)))
-                if (enemy_pos_next_turn - my_pos_next_turn).norm() < 2:
+                enemy_cone_pos = enemy.position + enemy.stats.attack_range * Vector(cos(radians(enemy.angle)), sin(radians(enemy.angle))) * 0.75
+                if (enemy_pos_next_turn - my_pos_next_turn).norm() < 4 or (enemy_cone_pos - my_pos_next_turn).norm() < 4:
                     response[id] = steer_away_from_point(plane, enemy_pos_next_turn)
                     turned_away = True
                     break
-                
-            if turned_away:
-                continue
+
+            if steer_crashes_plane(response[id], plane):
+                print("don't kill yourself",)
+                response[id] *= -1
+                if id in hunted_targets:
+                    del hunted_targets[id]
 
         return response
 
